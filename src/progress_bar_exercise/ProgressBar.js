02644ad5a2d2ffa7cabd1f6ac06e3ed288df4674
poisongrape <10935061+poisongrape@users.noreply.github.com>
2021-07-30 19:26:44 -0700

    Add "perc" to ProgressBar and ProgressBarFiller.
    Pass down percentage to ProgressBar and ProgressBarFiller. The Filler's
    width changes based on percentage.

--- src/progress_bar_exercise/ProgressBar.js
+++ src/progress_bar_exercise/ProgressBar.js
@@ -1,13 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import ProgressBarFiller from "./ProgressBarFiller";
 
 // Progress bar frame.
-const ProgressBar = ({}) => {
+const ProgressBar = ({
+  perc,
+}) => {
   return (
     <div className="progress-bar">
-      <ProgressBarFiller />
+      <ProgressBarFiller perc={perc} />
     </div>
   );
+};
+
+ProgressBar.propTypes = {
+  /** Percentage that progress bar is completed. */
+  perc: PropTypes.number.isRequired,
+};
+
+ProgressBar.defaultProps = {
 };
 
 export default ProgressBar;
--- src/progress_bar_exercise/ProgressBarFiller.js
+++ src/progress_bar_exercise/ProgressBarFiller.js
@@ -1,10 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 // Filler content for progress bar.
-const ProgressBarFiller = ({}) => {
+const ProgressBarFiller = ({
+  perc,
+}) => {
+  const percentageStr = perc + "%";
+
   return (
-    <div className="filler" />
+    <div
+      className="filler"
+      style={{ width: `${percentageStr}`}}
+    />
   );
+};
+
+ProgressBarFiller.propTypes = {
+  /** Percentage that progress bar is completed. */
+  perc: PropTypes.number.isRequired,
+};
+
+ProgressBarFiller.defaultProps = {
 };
 
 export default ProgressBarFiller;
