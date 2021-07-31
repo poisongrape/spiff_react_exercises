fda98c29bd460326631dc175a0c7cb8e190fc803
poisongrape <10935061+poisongrape@users.noreply.github.com>
2021-07-30 19:49:18 -0700

    Finish progress when "Finish" is clicked.
    Add onClick handler to "Finish" button. It will animate the progress
    bar to 100% in 1 second.

--- src/progress_bar_exercise/ProgressBarExercise.js
+++ src/progress_bar_exercise/ProgressBarExercise.js
@@ -22,31 +22,44 @@ export default ProgressBarExercise;
 const Solution = () => {
   // Variables
   const interval = 1000;
+  const finishInterval = 100;
 
   // State
   const [percentage, setPercentage] = useState(0);
   const [count, setCount] = useState(0); // Time in seconds.
   const [timer, setTimer] = useState();
+  const [finishCount, setFinishCount] = useState(0);
+  const [finishTimer, setFinishTimer] = useState();
 
   // Effects
 
-  // Clear timer when component unmounts.
+  // Clear timers when component unmounts.
   useEffect(() => {
     return () => {
       if (typeof timer !== "undefined") {
         window.clearInterval(timer);
+      }
+
+      if (typeof finishTimer !== "undefined") {
+        window.clearInterval(finishTimer);
       }
     }
   }, [timer]);
 
   // Stop timer when it has ran for 15 seconds.
   useEffect(() => {
-    if (count >= 15) {
-      if (timer) {
-        window.clearInterval(timer);
-      }
+    if (count >= 15 && timer) {
+      window.clearInterval(timer);
     }
   }, [count])
+
+  // Stop finish request after 1 second.
+  useEffect(() => {
+    if (finishCount >= 10 && finishTimer) {
+      window.clearInterval(finishTimer);
+      setPercentage(100);
+    }
+  }, [finishCount])
 
   /**
    * Initiate fake request.
@@ -54,12 +67,12 @@ const Solution = () => {
    * For simplicity, increment progress bar every second at an even rate.
    */
   const handleStartClick = () => {
-    if (timer) {
+    if (typeof timer !== "undefined") {
       // We already started a timer, so do not do anything.
       return;
     }
 
-    const timerId = setInterval(() => {
+    const timerId = window.setInterval(() => {
       setPercentage((prevPercentage) => {
         return prevPercentage + (90 / 15);
       });
@@ -71,11 +84,35 @@ const Solution = () => {
     setTimer(timerId);
   };
 
+  /**
+   * Stop the fake request.
+   * Animate bar to 100% in 1 second, regardless of its current position.
+   * To make animation smooth, we divide interval into 10 chunks of 100 milliseconds.
+   */
+  const handleFinishClick = () => {
+    if (typeof timer !== "undefined") {
+      window.clearInterval(timer);
+
+      const remainingPercentage = 100 - percentage;
+
+      const finishTimerId = window.setInterval(() => {
+        setPercentage((prevPercentage) => {
+          return prevPercentage + (remainingPercentage / 10);
+        });
+        setFinishCount((prevCount) => {
+          return prevCount + 1;
+        });
+      }, finishInterval);
+
+      setFinishTimer(finishTimerId);
+    }
+  };
+
   return (
     <React.Fragment>
       <ProgressBar perc={percentage} />
       <Button label="Start Request" color="green" onClick={handleStartClick} />
-      <Button label="Finish Request" color="red" />
+      <Button label="Finish Request" color="red" onClick={handleFinishClick} />
     </React.Fragment>
   );
 };
