5eb436d62931fab42fd23059767a4bace9bbe01a
poisongrape <10935061+poisongrape@users.noreply.github.com>
2021-07-30 17:11:10 -0700

    Create foundation for Progress Bar and Filler.
    Progress Bar component is the frame for the progress bar.
    
    The Filler component is the color that fills the progress bar.

---
+++ src/progress_bar_exercise/ProgressBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProgressBarFiller from "./ProgressBarFiller";
+
+// Progress bar frame.
+const ProgressBar = ({}) => {
+  return (
+    <div className="progress-bar">
+      PROGRESS BAR
+      <ProgressBarFiller />
+    </div>
+  );
+};
+
+export default ProgressBar;
---
+++ src/progress_bar_exercise/ProgressBarFiller.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Filler content for progress bar.
+const ProgressBarFiller = ({}) => {
+  return (
+    <div className="filler">FILLER</div>
+  );
+};
+
+export default ProgressBarFiller;
