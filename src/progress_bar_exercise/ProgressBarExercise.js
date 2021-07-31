ef9bf96b6ca1fc3558ac445f3dd52eab9b05b771
poisongrape <10935061+poisongrape@users.noreply.github.com>
2021-07-30 19:33:38 -0700

    Animate progress bar on "Start" button click.
    When user clicks on "Start" button, start incrementing the progress
    bar.
    
    We hardcode the progress bar to increment at an even rate over 15
    seconds, at which point it will hang at 90% complete.

--- src/progress_bar_exercise/ProgressBarExercise.js
+++ src/progress_bar_exercise/ProgressBarExercise.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Exercise from "../exercise/Exercise";
 import ProgressBar from "./ProgressBar";
 import Button from "./Button";
@@ -20,10 +20,61 @@ export default ProgressBarExercise;
 // ----------------------------------------------------------------------------------
 
 const Solution = () => {
+  // Variables
+  const interval = 1000;
+
+  // State
+  const [percentage, setPercentage] = useState(0);
+  const [count, setCount] = useState(0); // Time in seconds.
+  const [timer, setTimer] = useState();
+
+  // Effects
+
+  // Clear timer when component unmounts.
+  useEffect(() => {
+    return () => {
+      if (typeof timer !== "undefined") {
+        window.clearInterval(timer);
+      }
+    }
+  }, [timer]);
+
+  // Stop timer when it has ran for 15 seconds.
+  useEffect(() => {
+    if (count >= 15) {
+      if (timer) {
+        window.clearInterval(timer);
+      }
+    }
+  }, [count])
+
+  /**
+   * Initiate fake request.
+   * Start animating progress bar. It will reach 90% at 15 seconds.
+   * For simplicity, increment progress bar every second at an even rate.
+   */
+  const handleStartClick = () => {
+    if (timer) {
+      // We already started a timer, so do not do anything.
+      return;
+    }
+
+    const timerId = setInterval(() => {
+      setPercentage((prevPercentage) => {
+        return prevPercentage + (90 / 15);
+      });
+      setCount((prevCount) => {
+        return prevCount + 1;
+      });
+    }, interval);
+
+    setTimer(timerId);
+  };
+
   return (
     <React.Fragment>
-      <ProgressBar />
-      <Button label="Start Request" color="green" />
+      <ProgressBar perc={percentage} />
+      <Button label="Start Request" color="green" onClick={handleStartClick} />
       <Button label="Finish Request" color="red" />
     </React.Fragment>
   );
